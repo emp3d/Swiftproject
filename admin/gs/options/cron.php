160597e78455c8d2e72d58688b464e6949472db9
Janno Esko <eskojanno@gmail.com>
2015-06-04 23:38:57 +0300

    Cron job for Linux
    Cron job for linux works, keeps the servers alive.

--- admin/gs/options/cron.php
+++ admin/gs/options/cron.php
@@ -1,25 +1,30 @@
 #!/usr/bin/php
 <?php
-    include 'server.php';
-    $mysql = include '../../../config.php';
-    $query = "SELECT swift_servers.account AS account, swift_servers.password AS accpass, swift_servers.script AS startcmd, swift_servers.port AS gameport, swift_hosts.ip AS hostIp, swift_hosts.sshport AS sshport FROM swift_servers, swift_hosts WHERE swift_servers.active=1 AND swift_servers.host_id = swift_hosts.id";
+    include '/var/www/swiftproject/admin/gs/options/server.php';
+    $mysql = include '/var/www/swiftproject/config.php';
+    $query = "SELECT swift_servers.account AS account, swift_servers.name AS srvname, swift_servers.password AS accpass, swift_servers.script AS startcmd, swift_servers.port AS gameport, swift_hosts.ip AS hostIp, swift_hosts.sshport AS sshport FROM swift_servers, swift_hosts WHERE swift_servers.active=1 AND swift_servers.host_id = swift_hosts.id";
     $result = mysqli_query($mysql, $query);
     while ($row = mysqli_fetch_array($result)) {
-        die("I made it here while I shouldn't have!");
+//        die("I made it here while I shouldn't have!");
         $account = trim($row['account']);
         $accpass = trim($row['accpass']);
         $startcmd = trim($row['startcmd']);
         $gameport = intval(trim($row['gameport']));
         $hostIp = trim($row['hostIp']);
+	$server = $row['srvname'];
         $sshport = intval(trim($row['sshport']));
+	echo "Checking server $server \n";
         if (!checkStatus($hostIp, $sshport, $account, $accpass)) {
             //screen down, start it up again.
-            echo "Screen down, restarting...\n";
+            echo "Screen for server $server is down, restarting...\n";
             startServer($hostIp, $sshport, $account, $accpass, $startcmd);
         } else if (!checkServer($hostIp, $gameport)) {
             //screen up, server down, start it up again
-            echo "Server down, restarting...\n";
+            echo "Server $server is down, restarting...\n";
             restartServer($hostIp, $sshport, $account, $accpass, $startcmd);
-        }
+        } else {
+	    echo "Server $server is running...\n";
+	}
     }
 ?>
+
